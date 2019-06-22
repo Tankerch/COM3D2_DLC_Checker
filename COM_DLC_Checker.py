7a92409fc1122d55a5e2f55502f4b482d0000719
Tankerch <ayemjang2@gmail.com>
2019-06-22 11:07:54 +0700

    Add files via upload
    FUK KISS WITH THEIR UPDATE.LST

--- COM_DLC_Checker.py
+++ COM_DLC_Checker.py
@@ -1,7 +1,7 @@
 #Version 3, Using update from repo
 import time
 import requests
-import os.path
+import os
 from colorama import init
 from termcolor import colored, cprint
 init()
@@ -11,7 +11,7 @@ start = time.time()
 #Check connection
 #if connection is available, Check Update DLC list from repo
 #Write new DLC list if current DLC list is old
-url='https://raw.githubusercontent.com/Tankerch/COM3D2_DLC_Checker/master/COM_listDLC.lst'
+url='https://raw.githubusercontent.com/Tankerch/COM3D2_DLC_Checker/master/COM_NewListDLC.lst'
 def check_internet():
     timeout=3
     try:
@@ -23,45 +23,48 @@ def check_internet():
 
 if check_internet():
     r = requests.get(url, timeout=3)
-    if os.path.isfile('COM_listDLC.lst'):
-        with open('COM_listDLC.lst', 'r') as f:
+    if os.path.isfile('COM_NewListDLC.lst'):
+        with open('COM_NewListDLC.lst', 'r') as f:
             first_line_DLC = int(f.readline().rstrip('\n'))
         first_line_update = r.text.splitlines()[0]
         if first_line_update != "404: Not Found":
             first_line_update = int(first_line_update)
             if first_line_update > first_line_DLC:
-                with open('COM_listDLC.lst', 'wb') as f:
+                with open('COM_NewListDLC.lst', 'wb') as f:
                     f.write(r.content)
         else:
-            input("Failed download DLC list, Contact author")
+            input("Failed to download DLC list, please contact author")
             exit()
     else:
         if r.text.splitlines()[0] != "404: Not Found":
-            with open('COM_listDLC.lst', 'wb') as f:
+            with open('COM_NewListDLC.lst', 'wb') as f:
                 f.write(r.content)
         else:
-            input("Failed download DLC list, Contact author")
+            input("Failed to download DLC list, please contact author")
             exit()
 else:
-    if not os.path.isfile('COM_listDLC.lst'):
-        input("COM_listDLC.lst isn't exist, please connect to internet to auto-download it")
+    if not os.path.isfile('COM_NewListDLC.lst'):
+        input("COM_NewListDLC.lst doesn't exist, please connect to the internet redownload it")
         exit()
 
 #Start
-print(colored("================================================================================================================", 'cyan',attrs=['bold']))
-print(colored('COM_DLC_Checker by Tankerch', 'cyan',attrs=['bold']))
-print(colored("================================================================================================================", 'cyan',attrs=['bold']))
+print(colored("===========================================================================================", 'cyan',attrs=['bold']))
+print(colored('COM_DLC_Checker', 'cyan',attrs=['bold']))
+print(colored("===========================================================================================", 'cyan',attrs=['bold']))
 
 #Open file and removing header for DLC List
 line_inform = []
-with open('COM_listDLC.lst', 'r') as f:
+with open('COM_NewListDLC.lst', 'r') as f:
     for _ in range(1):
         next(f)
     for line in f:
         line_inform.append(line.rstrip('\n').split(","))
 line_DLC = set(list(zip(*line_inform))[0])
 line_informset = set(list(zip(*line_inform))[1])
-line_Real = set(line.strip().split(",")[0] for line in open('Update.lst', 'r'))
+
+#Make a set from gamedata
+line_Real = set(os.listdir("GameData"))
+line_Real.update(os.listdir("GameData_20"))
 
 #Searching with intersection and linear remove searching
 count_p = set()
