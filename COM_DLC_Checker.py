e03960aaea14df90ca2d1287a41ea2021a3c1d66
Tankerch <ayemjang2@gmail.com>
2019-09-24 20:21:34 +0700

    Update COM_DLC_Checker.py

--- COM_DLC_Checker.py
+++ COM_DLC_Checker.py
@@ -1,4 +1,4 @@
-#Version 3, Using update from repo
+#Version 4, More user friendly, I guess ¯\_(ツ)_/¯
 import time
 import requests
 import os
@@ -8,12 +8,10 @@ init()
 
 start = time.time()
 
-#Check the internet connection
-#if the connection is available, check update DLC list from repositories.
-#Overwrite new DLC list if current DLC list is the old version
+#Request function
 url='https://raw.githubusercontent.com/Tankerch/COM3D2_DLC_Checker/master/COM_NewListDLC.lst'
 def check_internet():
-    timeout=3
+    timeout=2
     try:
         requests.get(url, timeout=timeout)
         return True
@@ -21,21 +19,34 @@ def check_internet():
         pass
     return False
 
+#Start
+print(colored("===========================================================================================", 'cyan',attrs=['bold']))
+print(colored('COM_DLC_Checker', 'cyan',attrs=['bold']) + " | " + colored(' Github.com/Tankerch/COM3D2_DLC_Checker', 'cyan',attrs=['bold']))
+print(colored("===========================================================================================", 'cyan',attrs=['bold']))
+print("Checking internet connection : ")
+
+#Check connection
+#if connection is available, Check Update DLC list from repo
+#Write new DLC list if current DLC list is old
 if check_internet():
-    r = requests.get(url, timeout=3)
-    if os.path.isfile('COM_NewListDLC.lst'):
-        with open('COM_NewListDLC.lst', 'r') as f:
+    r = requests.get(url, timeout=2)
+    if os.path.isfile('COM_NewListDLC.lst'): #Check offline DLC list
+        print("Connected")
+        with open('COM_NewListDLC.lst', 'r') as f: #Read offline version header of DLC list
             first_line_DLC = int(f.readline().rstrip('\n'))
         first_line_update = r.text.splitlines()[0]
         if first_line_update != "404: Not Found":
-            first_line_update = int(first_line_update)
-            if first_line_update > first_line_DLC:
+            first_line_update = int(first_line_update) #Read online version header of DLC list
+            if first_line_update > first_line_DLC: #Found Update
+                print("Found update, updating DLC list")
                 with open('COM_NewListDLC.lst', 'wb') as f:
                     f.write(r.content)
-        else:
+            else: #No update
+                print("DLC list is up-to-date")
+        else: #There's online file, but failed to load the actual content
             input("Failed to download DLC list, please contact author")
             exit()
-    else:
+    else: #No offline DLC list
         if r.text.splitlines()[0] != "404: Not Found":
             with open('COM_NewListDLC.lst', 'wb') as f:
                 f.write(r.content)
@@ -43,16 +54,12 @@ if check_internet():
             input("Failed to download DLC list, please contact author")
             exit()
 else:
-    if not os.path.isfile('COM_NewListDLC.lst'):
-        input("COM_NewListDLC.lst doesn't exist, please connect to the internet redownload it")
+    if not os.path.isfile('COM_NewListDLC.lst'): #No connection + offline DLC list
+        input("COM_NewListDLC.lst doesn't exist, Connect to the internet redownload it")
         exit()
 
-#Start
-print(colored("===========================================================================================", 'cyan',attrs=['bold']))
-print(colored('COM_DLC_Checker', 'cyan',attrs=['bold']))
-print(colored("===========================================================================================", 'cyan',attrs=['bold']))
-
-#Open file and removing header in DLC List
+print("\nBegin sorting, Please wait a moment")
+#Open file and removing header for DLC List
 line_inform = []
 with open('COM_NewListDLC.lst', 'r') as f:
     for _ in range(1):
@@ -62,32 +69,38 @@ with open('COM_NewListDLC.lst', 'r') as f:
 line_DLC = set(list(zip(*line_inform))[0])
 line_informset = set(list(zip(*line_inform))[1])
 
-#Make a set from gamedata folder
-line_Real = set(os.listdir("GameData"))
-line_Real.update(os.listdir("GameData_20"))
+#Check + make a set from gamedata
+error = False
+try:
+    line_Real = set(os.listdir("GameData"))
+    line_Real.update(os.listdir("GameData_20"))
+except Exception:
+	print("Error : Make sure to run this program in COM3D2 directories")
+	error = True
 
-#Searching with intersection and linear methods
-count_p = set()
-for x in line_DLC.intersection(line_Real):
-    for y in line_inform:
-        if x == y[0]:
-            count_p.add(y[1])
-            line_inform.remove(y)
-            break
+if(not error):
+	#Searching with intersection and linear remove searching
+	count_p = set()
+	for x in line_DLC.intersection(line_Real):
+		for y in line_inform:
+			if x == y[0]:
+				count_p.add(y[1])
+				line_inform.remove(y)
+				break
 
-#Separating DLC that installed with not installed
-count_n = line_informset.difference(count_p)
+	#Separating installed with not installed
+	count_n = line_informset.difference(count_p)
 
-#Show time
-print(colored('Already Installed:', 'cyan',attrs=['bold']))
-for x in sorted(count_p):
-    print(x)
+	#Show time
+	print('\n'+colored('Already Installed:', 'cyan',attrs=['bold']))
+	for x in sorted(count_p):
+		print(x)
 
-print(colored("\nNot Installed:", 'cyan',attrs=['bold']))
-for x in sorted(count_n):
-    print(x)
+	print(colored("\nNot Installed:", 'cyan',attrs=['bold']))
+	for x in sorted(count_n):
+		print(x)
 
-#End
+#Ending & Note
 end = time.time()
 print("\n\nElapsed time:", end-start)
 text = input("Press Enter to end program")
